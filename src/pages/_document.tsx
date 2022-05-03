754540b717f02935ba1c62a38dbd3220cb1ab712
robertokbr <robertojuniordev@gmail.com>
2022-05-03 17:03:56 -0300

    feat: add fonts link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"></link>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,500;1,600&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"></link>
           <link rel="shortcut icon" href="/static/favicon.png" sizes='96x96' type="image/png" />
         </Head>
         <body>
