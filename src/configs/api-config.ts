a35a39dc5b2cd090b564e9bc4349cb359fa3cd2f
robertokbr <robertojuniordev@gmail.com>
2022-05-03 17:01:37 -0300

    chore: rename config files

---
+++ src/configs/api-config.ts
@@ -0,0 +1,3 @@
+export const apiConfig = {
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://0.0.0.0:3333'
+}
--- src/configs/api.ts
+++
@@ -1,3 +0,0 @@
-export const apiConfig = {
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || 'http://0.0.0.0:3333'
-}
