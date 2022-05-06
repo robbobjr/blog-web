3840b1066314d686e6034e56a32846c882e369c8
robertokbr <robertojuniordev@gmail.com>
2022-05-06 11:36:32 -0300

    chore: add route redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: () => {
+    return [
+      {
+        source: '/',
+        destination: '/ptbr',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
