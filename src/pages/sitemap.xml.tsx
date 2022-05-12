734b3a042531463c79b041a712214de7d5667a01
robertokbr <robertojuniordev@gmail.com>
2022-05-12 19:27:39 -0300

    chore: update sitemap

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -2,7 +2,7 @@ import { appConfig } from "../configs/app-config";
 import { AxiosAPI } from "../services/api/axios";
 import { PostDto, PostTagDto } from "../services/api/openapi";
 
-function generateSiteMap(posts: PostDto[], tags: PostTagDto[]) {
+function generateSiteMap(posts: PostDto[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      ${posts
@@ -14,15 +14,6 @@ function generateSiteMap(posts: PostDto[], tags: PostTagDto[]) {
      `;
        })
        .join('')}
-      ${tags
-      .map(({ name }) => {
-        return `
-      <url>
-          <loc>${`${appConfig.baseURL}/${name}`}</loc>
-      </url>
-    `;
-      })
-      .join('')}
    </urlset>
  `;
 }
@@ -34,10 +25,10 @@ function SiteMap() {
 export async function getServerSideProps({ res }) {
   // We make an API call to gather the URLs for our site
   const client = new AxiosAPI("SiteMap:getServerSideProps"); 
-  const { posts, tags } = await client.getPostsAndTags({});
+  const posts = await client.getPosts();
 
   // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts, tags);
+  const sitemap = generateSiteMap(posts);
 
   res.setHeader('Content-Type', 'text/xml');
   // we send the XML to the browser
