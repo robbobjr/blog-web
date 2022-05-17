24c41d0bba1f27e7450dc62e9592cf9dadac2489
robertokbr <robertojuniordev@gmail.com>
2022-05-17 13:26:28 -0300

    feat: add portfolio page

--- src/components/molecules/footers/copyright-footer/index.tsx
+++ src/components/molecules/footers/copyright-footer/index.tsx
@@ -1,9 +1,12 @@
 import { Text } from "@chakra-ui/react";
+import { Link } from "../../../atoms/link";
 
 export function CopyrightFooter() {
   return (
-    <Text fontSize="12" fontWeight="thin" opacity="0.5" color="gray.50" textAlign="center">
-      © 2022 Roberto Junior
-    </Text>
+    <Link href="/portfolio">
+      <Text fontSize="12" fontWeight="thin" opacity="0.5" color="gray.50" textAlign="center">
+        © 2022 Roberto Junior
+      </Text>
+    </Link>
   ); 
 }
