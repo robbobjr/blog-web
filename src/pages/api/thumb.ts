1351021d34aace80a39775ac64b23340e0492560
robertokbr <robertojuniordev@gmail.com>
2022-05-19 20:21:17 -0300

    build: add canvas fix script

--- src/pages/api/thumb.ts
+++ src/pages/api/thumb.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createCanvas } from 'canvas';
+import { createCanvas, registerFont } from 'canvas';
 
 export default async function handler(
   req: NextApiRequest, 
   res: NextApiResponse
   ) {
   try {
+    registerFont('./public/static/JetBrainsMono-Regular.ttf', { family:  'JetBrainsMono' })
     const { title } = req.query;
     const canvas = createCanvas(1200, 630);
     const context = canvas.getContext('2d');
@@ -13,7 +14,7 @@ export default async function handler(
     context.fillStyle="#1d1e26";
     context.fillRect(0, 0, canvas.width, canvas.height);
     
-    context.font = `10rem Sans`
+    context.font = `10rem JetBrainsMono`
     context.textAlign ="center";
     context.fillStyle="#ff79c6";
     context.fillText(title as string, canvas.width / 2, canvas.height / 2);
