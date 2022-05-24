9b2d3d1f6fe9f3566b651b5be5274e05f8ae3242
robertokbr <robertojuniordev@gmail.com>
2022-05-24 14:48:34 -0300

    chore: add easter-egg-log util

---
+++ src/utils/easter-egg-log.ts
@@ -0,0 +1,4 @@
+export const easterEggLog = {
+  content: "%cFollow me, I'm the pied piper 🎶",
+  style: "color: #ff79c6; font-size: 4rem; padding: 1rem; background: #282a36; border-radius: 1rem",
+}
