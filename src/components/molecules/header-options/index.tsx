d7788fd81b4d92785f970983ba2ed0f970b0a727
robertokbr <robertojuniordev@gmail.com>
2022-06-06 15:57:10 -0300

    chore: remove icon

--- src/components/molecules/header-options/index.tsx
+++ src/components/molecules/header-options/index.tsx
@@ -1,31 +1,12 @@
 import { HStack } from "@chakra-ui/react";
-import { AiOutlineCaretUp } from "react-icons/ai";
 import { GoPlus } from "react-icons/go";
 import { UserDto } from "../../../services/api/openapi";
 import { useAuth } from "../../../states/hooks/use-auth";
 import { HeaderIcon } from "../../atoms/header-icon";
 import { CreatePostModal } from "../../organisms/create-post-modal";
-import { useCallback, useState } from "react";
-import { useContent } from "../../../states/hooks/use-content";
-import { useRouter } from "next/router";
 
 export function HeaderOptions() {
-  const [isPressed, setIsPressed] = useState(false);
   const { data } = useAuth(); 
-  const { handleSearchPosts } = useContent();
-  const history = useRouter();
-
-  const handleUserLikedPosts = useCallback(async () => {
-    if (!data?.user) return;
-    setIsPressed(state => !state);
-    await handleSearchPosts(
-      isPressed 
-      ? undefined 
-      : { userId: `${data?.user?.id}`, rateValue: "1" }
-    );
-    if (history.pathname !== "/") return history.push("/");
-  }, [data?.user, handleSearchPosts, history, isPressed]);
-  
 
   return (
     <HStack
@@ -37,13 +18,6 @@ export function HeaderOptions() {
       ml={{ sm: 'auto', md: "8" }}
       display={{ base: "none", sm: "inherit" }}
     >
-      {data && (
-        <HeaderIcon 
-          isPressed={isPressed}
-          icon={AiOutlineCaretUp} 
-          onClick={handleUserLikedPosts}
-        />
-      )}
       {data?.user?.role === UserDto.role.ADMIN && (
         <CreatePostModal>
           <HeaderIcon icon={GoPlus} />
