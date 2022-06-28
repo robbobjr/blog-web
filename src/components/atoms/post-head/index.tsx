a5b4678482911583159c8f0fda8a2c19c2109d3b
robertokbr <robertojuniordev@gmail.com>
2022-06-28 00:04:22 -0300

    chore: add default post image

--- src/components/atoms/post-head/index.tsx
+++ src/components/atoms/post-head/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { appConfig } from "../../../configs/app-config";
+import { metaConfig } from "../../../configs/meta-config";
 
 export function PostHead({ data }) {
   return (
@@ -13,14 +14,14 @@ export function PostHead({ data }) {
       <meta property="og:url" content={`${appConfig.baseURL}/post/${data.slug}`}/>
       <meta property="og:title" content={data.title}/>
       <meta property="og:description" content={data.description}/>
-      <meta property="og:image" content={data?.image}/>
+      <meta property="og:image" content={data?.image || metaConfig.feed.image}/>
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content={`${appConfig.baseURL}/post/${data.slug}`}/>
       <meta property="twitter:title" content={data.title}/>
       <meta property="twitter:description" content={data.description}/>
-      <meta property="twitter:image" content={data?.image}/>
+      <meta property="twitter:image" content={data?.image || metaConfig.feed.image}/>
     </Head>
   );
 }
