d361206349837b9782e66179ba11affabca960ea
robkbr <roberto.junior@nickelpay.com.br>
2022-08-02 22:08:44 -0300

    feat: add bio component

--- src/components/atoms/link.tsx
+++ src/components/atoms/link.tsx
@@ -1,3 +1,4 @@
+import { LinkBoxProps } from "@chakra-ui/react";
 import NextLink, { LinkProps } from "next/link";
 import { ReactElement } from "react";
 
@@ -5,11 +6,12 @@ export function Link({
   href, 
   children, 
   target,
+  aProps,
   ...props 
-}: LinkProps & { children: ReactElement[] | ReactElement, target?: string }) {
+}: LinkProps & { children: any, target?: string, aProps?: any }) {
   return (
     <NextLink passHref={true} href={href} {...props}>
-      <a target={target}>
+      <a target={target} style={{...aProps}}>
         {children}
       </a>
     </NextLink>
